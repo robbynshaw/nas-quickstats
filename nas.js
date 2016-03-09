2a2c380d99b4d98e537afb6e9956d7277f109a17
Robby Shaw <robbynshaw@gmail.com>
2016-03-08 23:13:36 -0600

    Got a simple test query running

--- nas.js
+++ nas.js
@@ -1,9 +1,16 @@
 var qs = require('querystring');
+var http = require('http');
 
 module.exports = (function(){
     var nas = {};
     var key = 'C47BE18E-7811-30AD-BF3D-2D93716356CE';
-    var host = 'http://quickstats.nass.usda.gov';
+    var host = 'quickstats.nass.usda.gov';
+    
+    nas.type = {
+        get: 0,
+        params: 1,
+        counts: 2
+    };
 
     nas.newQuery = function(){
         return {
@@ -12,15 +19,37 @@ module.exports = (function(){
         };
     };
 
-    var genCallback = function(response, callback) {
-        var s = '';
-        response.on('data', function(chunk){
-            s += chunk;
+    nas.testYield = function(){
+        var q = nas.yieldQuery();
+        var r = nas.request(nas.type.get, q, function(data){
+            var d = JSON.parse(data);
+            var all = d.data;
+            console.log(all[0]);
+            for (var i=0; i<all.length; i++){
+                var yearly = all[i];
+                console.log('YEAR: ' + yearly.year);
+                console.log('\t' + yearly.Value + ' ' + yearly.unit_desc);
+            }
         });
-        response.on('end', function(){
-            console.log(s);
-            callback(s);
-        });
+        r.send();
+    };
+
+    var genCallback = function(callback){
+        var f = function(response) {
+            var s = '';
+            //response.on('answer', function(data){
+            //    console.log(data);
+            //})
+            response.on('data', function(chunk){
+                s += chunk;
+            });
+            response.on('end', function(){
+                if (callback){
+                    callback(s);
+                }
+            });
+        };
+        return f;
     };
 
     nas.yieldQuery = function(commodity){
@@ -31,34 +60,38 @@ module.exports = (function(){
         return q;
     };
 
-    nas.request = function(type, query){
-        var path = createPath(type, query);
-        var options = {
-            host: host,
-            path: path
-        }
+    nas.request = function(type, query, callback){
+        var options = createOptions(type, query);
+        var newCallback = genCallback(callback);
         return {
-            send: function(callback){
-                jsonp(urlStr, callback);
+            options: options,
+            send: function(){
+                http.request(options, newCallback).end();
             }
         };
     };
 
 
-    function createPath(type, query){
+    function createOptions(type, query){
         var apiType = '';
         switch (type){
-            case 'get':
+            case nas.type.get:
                 apiType = '/api/api_GET/';
                 break;
-            case 'params':
+            case nas.type.params:
                 apiType = '/api/get_param_values/';
                 break;
-            case 'counts':
+            case nas.type.counts:
                 apiType = '/api/get_counts/';
                 break;
+            default:
+                console.error('An incorrect type was passed to' +
+                             ' the api request');
         }
-        return apiType + '?' + qs.stringify(query);
+        return {
+            host: host,
+            path: apiType + '?' + qs.stringify(query)
+        };
     }
 
     return nas;
